17fc97dad12129703ffd4287ec3c2f949d85bc37
sabrinagiuliani <sabrina2002giuliani@gmail.com>
2023-02-23 10:09:30 +0100

    Homepage!!!!!!!!!!!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,27 @@
 @endsection
 
 @section('body')
-    CIAO QUESTA è LA HOME DI TAXATIO
+
+    <div class="background-homepage">
+        <img src="{{asset('/Asset/img/sfondologin2.jpg')}}" alt="immagine di sfondo hero login" class="z-1">
+        <div class="sfondo-login-overlay z-15"></div>
+        
+        <div class="container-home">
+            <h1><i>Benvenuto in</i></h1>
+            <img src="{{asset('/Asset/img/logoTaxatio.png')}}" alt="Logo Taxatio">  
+        </div>
+        
+        <div class="testo-home">
+            <p>La nostra applicazione per valutare i docenti! Siamo molto colorati e propositivi</p>
+        </div>
+        
+        <div class="pulsanti-home">
+            <div class="pulsanti-assoluti">
+                <a href="/login" class="inline-block text-center"><div class="pulsante-accedi">Accedi</div></a>
+                <a href="/register" class="inline-block text-center"><div class="pulsante-registrati">Registrati</div></a>
+            </div>
+        </div>
+
+
+    </div>
 @endsection
